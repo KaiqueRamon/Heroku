7360d6bf472d57ccc729ed6bf8ce6e24865c62fc
KaiqueRamon <kaique.dantas@sou.inteli.edu.br>
2022-06-15 09:57:43 -0300

    curriculo

---
+++ semana 7/curriculo/script.js
@@ -0,0 +1,20 @@
+$("#clicavel").click(function(){
+  $("#trabalho1").toggle(1000);
+});
+
+$("#clicavel2").click(function(){
+  $("#trabalho2").toggle(1000);
+});
+
+$("#clicavel3").click(function(){
+  $("#trabalho3").toggle(1000);
+});
+
+$("#clicavel4").click(function(){
+  $("#trabalho4").toggle(1000);
+});
+
+$("#clicavel5").click(function(){
+  $("#contrato").toggle(500);
+  $('#contrato').css({'color': 'blue'}); 
+})
